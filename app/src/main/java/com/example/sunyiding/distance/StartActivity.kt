ef0e8d93b224f562295b865a52cc1007407e2c2b
yiding-sun <sunyidingman@foxmail.com>
2018-07-29 09:18:34 +0800

    add start activity

---
+++ app/src/main/java/com/example/sunyiding/distance/StartActivity.kt
@@ -0,0 +1,18 @@
+package com.example.sunyiding.distance
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class StartActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+        findViewById<Button>(R.id.choose_photo).setOnClickListener {
+            val intent=Intent(this@StartActivity,Class.forName("com.example.sunyiding.distance.MainActivity"))
+            startActivity(intent)
+        }
+    }
+}
